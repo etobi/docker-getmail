af9eb770a3f853a02758254da3104227ea729c4d
Tobias Liebig <mail@etobi.de>
2022-03-30 00:14:49 +0200

    global sieve script for bogo

--- bin/start.sh
+++ bin/start.sh
@@ -2,6 +2,6 @@
 set -e
 
 /etc/init.d/cron start
-sievec /etc/dovecot/sieve/
+sievec /etc/dovecot/sieve/bogofilter.sieve
 /usr/sbin/dovecot -F 1> /var/log/stdout 2>&1
 tail -qf --follow=name --retry /var/log/stdout
