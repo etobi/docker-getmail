10fd705f2cc48c1ee39500cb56c68b98af344b4d
Tobias Liebig <mail@etobi.de>
2022-04-18 21:49:07 +0200

    compile user sieve scripts

--- bin/start.sh
+++ bin/start.sh
@@ -3,5 +3,6 @@ set -e
 
 /etc/init.d/cron start
 (ls -l /etc/dovecot/sieve/ && sievec /etc/dovecot/sieve/) || true
+(ls -l /srv/mail/*/Maildir/sieve/*.sieve && sievec /srv/mail/*/Maildir/sieve/*.sieve) || true
 /usr/sbin/dovecot -F 1> /var/log/stdout 2>&1
 tail -qf --follow=name --retry /var/log/stdout
