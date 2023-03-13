2bb9ec7ba94f791cd69768bc9bac8fc43553aa4b
Tobias Liebig <mail@etobi.de>
2023-03-13 23:15:35 +0100

    sievec

--- bin/start.sh
+++ bin/start.sh
@@ -2,7 +2,8 @@
 set -e
 
 /etc/init.d/cron start
-(ls -l /etc/dovecot/sieve/ && sievec /etc/dovecot/sieve/) || true
-# (ls -l /srv/mail/*/Maildir/sieve/*.sieve && sievec /srv/mail/*/Maildir/sieve/*.sieve) || true
+
+(find /srv/mail/*/Maildir/sieve/ -name *.sieve -print -exec sievec {} \;) || true
+
 /usr/sbin/dovecot -F 1> /var/log/stdout 2>&1
 tail -qf --follow=name --retry /var/log/stdout
